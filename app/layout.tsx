0f74a323f7f4a423dec32093605ef072a617082e
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-04-13 18:59:54 +0200

    add next13 metadata

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import { Nunito } from 'next/font/google'
+
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
+
+import ToasterProvider from '@/app/providers/ToasterProvider';
+
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
+
+export const metadata = {
+  title: 'Airbnb Next13 Clone',
+  description: 'Welcome to DylanP97 Airbnb Clone with Next.js and Tailwind CSS',
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
+
+const font = Nunito({
+  subsets: ['latin'],
+});
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
